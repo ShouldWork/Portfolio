96955983e685819fc43fede51ec28e61b6811188
Krysp <mrblocke@gmail.com>
2016-04-25 10:01:02 -0600

    End of day
    End of day Apr 25th

--- src/js/my_scripts.js
+++ src/js/my_scripts.js
@@ -1,40 +1,48 @@
-/**
+/*
  * Created by Krysp on 4/21/16.
  */
+
+
+
 var imgs = {1:"It's the final", 2:"count down", 3:"!!!!!!!",4:"tada"}
-var clicked = '0';
-var myBtn =  document.getElementById('navBtner').style;
-var text = ''
-function btnMove(){
-    if (clicked == '0') {
-        clicked = '1';
-        myBtn.transform = 'rotate(180deg)';
-        myBtn.left = '0px';
-        myBtn.color = 'black';
-    } else {
-        clicked = '0';
-        myBtn.transform = 'rotate(0deg)';
-        myBtn.left = '200px';
-        myBtn.color = 'white';
-    }
+var clicked = 1;
+var elemStyle =  document.getElementById('navBtner').style;
+var text = '';
+var clicked2 = 1;
+
+function btnMove() {
     $("#wrapper").toggleClass("toggled");
+    clicked = (clicked === 0) ? 1 : 0;
+    if (clicked === 0) {
+        elemStyle.transform = 'rotate(180deg)';
+        elemStyle.left = '0px';
+        elemStyle.color = 'black';
+    } else {
+        elemStyle.transform = 'rotate(0deg)';
+        elemStyle.left = '200px';
+        elemStyle.color = 'ghostWhite';
+    }
 }
 
-
-// $("#navBtner").click(function(e) {
-//     $("#wrapper").toggleClass("toggled");
-// });
-
+document.getElementById("noGo1").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("noGo2").addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById("noGo3").addEventListener("click", function(event){
+    event.preventDefault()
+});
 
 function moveMe(){
-   document.getElementById('sm-cont').style.position = 'absolute';
-   document.getElementById('sm-cont').style.top = '0px';
-   document.getElementById('sm-cont').style.right = '100px';
+    document.getElementById('sm-cont').style.position = 'absolute';
+    document.getElementById('sm-cont').style.top = '0px';
+    document.getElementById('sm-cont').style.right = '100px';
 }
 
 
 $(document).ready(function(){
-    $("p").click(function(){
+    $("sss").click(function(){
         $(this).hide();
     });
 });
