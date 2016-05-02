150d3208a0e87a6dff548ce813d0d3050fc7a37d
Krysp <mrblocke@gmail.com>
2016-05-02 10:25:45 -0600

    End of the day May 2nd at school updated Resume to load with the knownLang() function. Created langBox class for known languages.

--- js/my_scripts.js
+++ js/my_scripts.js
@@ -1,8 +1,6 @@
-/**
+/*
  * Created by Krysp on 4/21/16.
  */
-
-
 
 var imgs = {1:"It's the final", 2:"count down", 3:"!!!!!!!",4:"tada"}
 var clicked = '0';
@@ -42,6 +40,37 @@ function quoteClass(){
     }
 }
 
+
+var lang_ahk = {pname: 'AHK', level: 'Proficient', scaleNum: 6};
+var lang_css3 = {pname: 'CSS3', level: 'Familiar', scaleNum: 3};
+var lang_html5 = {pname: 'HTML5', level: 'Familiar', scaleNum: 3};
+var lang_javaScript = {pname: 'JavaScript', level: 'Familiar', scaleNum: 3};
+var lang_tsql = {pname: 'T-SQL', level: 'Familiar', scaleNum: 4};
+
+var languages = [lang_ahk, lang_css3, lang_html5, lang_javaScript, lang_tsql];
+
+
+function knownLang(id) {
+    for (var i = 0; i < languages.length; i++) {
+
+        if (languages[i].pname === id) {
+            var el =  document.getElementById(id);
+            var a = el.innerHTML;
+            if (a == id) {
+                el.innerHTML = ' You know how to program in ' + languages[i].pname + '. I would say you are ' + languages[i].level + ' in ' + languages[i].pname + '. I would give it a scale of ' + languages[i].scaleNum + ' out of 10!';
+                el.style.color = 'green';
+                el.style.fontSize = '100%';
+            } else {
+                el.innerHTML = id;
+                el.style.color = 'ghostWhite';
+                el.style.fontSize = "150%";
+            }
+            console.log('You know how to program in ' + languages[i].pname + '. I would say you are ' + languages[i].level + ' in ' + languages[i].pname + '. I would give it a scale of ' + languages[i].scaleNum + ' out of 10!')
+        }
+    }
+}
+
+
 document.getElementById("noGo1").addEventListener("click", function(event){
     event.preventDefault()
 });
@@ -52,3 +81,32 @@ document.getElementById("noGo3").addEventListener("click", function(event){
     event.preventDefault()
 });
 
+/*
+
+Reflector Examples
+
+var Reflector = function(obj) {
+    this.getProperties = function() {
+        var properties = [];
+        for (var prop in obj) {
+            if (typeof obj[prop] != 'function') {
+                properties.push(prop);
+            }
+        }
+        return properties;
+    }
+};
+
+
+var Reflector = function(obj) {
+    this.getAllMethods = function() {
+        var methods = [];
+        for (var method in obj) {
+            if (typeof obj[method] = 'function') {
+                methods.push(method);
+            }
+        }
+    };
+    return methods;
+};
+*/
